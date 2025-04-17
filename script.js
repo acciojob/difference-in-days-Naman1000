5ea313a829aa7819df7bd87b6375c7d3900a0a5c
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-04-17 18:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
-};
+	const d1= new Date(date1);
+	const d2= new Date(date2);
 
+	const utc1 = Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate);
+	const utc2 = Date.UTC(d2.getFullYear(), d2.getMonth(), d2.getDate);
+
+	const dif = utc2 - utc1;
+ 
+	const ms = 24*60*60*1000;
+
+	return dif/ms;
+	
+};  
+ 
 // Do not change the code below.
+
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
